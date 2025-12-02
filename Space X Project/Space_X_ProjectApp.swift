fab9169dae19e1965ebd2f51563015715de40cc1
TipsyContent <112686529+TipsyContent@users.noreply.github.com>
2025-12-02 18:08:22 +0100

    Initial Commit

---
+++ Space X Project/Space_X_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Space_X_ProjectApp.swift
+//  Space X Project
+//
+//  Created by Tipsy on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Space_X_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Space X ProjectTests/Space_X_ProjectTests.swift
@@ -0,0 +1,16 @@
+//
+//  Space_X_ProjectTests.swift
+//  Space X ProjectTests
+//
+//  Created by Tipsy on 02/12/2025.
+//
+
+import Testing
+
+struct Space_X_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
