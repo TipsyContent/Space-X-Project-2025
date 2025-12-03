f5ae5a6eb185d3abc19bd8cf44d760a806b8a667
TipsyContent <112686529+TipsyContent@users.noreply.github.com>
2025-12-03 20:54:00 +0100

    Update Models.swift

--- Space X Project/Models/Models.swift
+++ Space X Project/Models/Models.swift
@@ -20,7 +20,7 @@ struct Launch: Codable, Identifiable, Hashable {
     let cores: [Core]?
     let fairings: Fairings?
     
-    // Additional fields from API that were missing
+    // Additional fields from API
     let static_fire_date_utc: String?
     let static_fire_date_unix: Int?
     let tdb: Bool?
@@ -29,6 +29,7 @@ struct Launch: Codable, Identifiable, Hashable {
     let failures: [Failure]?
     let ships: [String]?
     let auto_update: Bool?
+    let launch_library_id: String?
     
     struct CrewMember: Codable, Hashable {
         let crew: String  // Crew ID
@@ -56,7 +57,7 @@ struct Launch: Codable, Identifiable, Hashable {
         let youtube_id: String?
         let article: String?
         let wikipedia: String?
-        let presskit: String?  // Added missing field
+        let presskit: String?
         
         struct Patch: Codable, Hashable {
             let small: String?
@@ -106,4 +107,153 @@ struct Launch: Codable, Identifiable, Hashable {
     static func == (lhs: Launch, rhs: Launch) -> Bool {
         lhs.id == rhs.id
     }
+}
+
+struct Rocket: Codable, Identifiable {
+    let id: String
+    let name: String
+    let type: String?
+    let active: Bool?
+    let description: String?
+    let company: String?
+    let country: String?
+    let height: Dimension?
+    let diameter: Dimension?
+    let mass: Mass?
+    let first_stage: Stage?
+    let second_stage: Stage?
+    let engines: Engine?
+    let cost_per_launch: Int?
+    let success_rate_pct: Double?
+    let first_flight: String?
+    let flickr_images: [String]?
+    let wikipedia: String?
+    
+    struct Dimension: Codable {
+        let meters: Double?
+        let feet: Double?
+    }
+    
+    struct Mass: Codable {
+        let kg: Double?
+        let lb: Double?
+    }
+    
+    struct Stage: Codable {
+        let thrust_sea_level: Thrust?
+        let thrust_vacuum: Thrust?
+        let thrust: Thrust?
+        let reusable: Bool?
+        let engines: Int?
+        let fuel_amount_tons: Double?
+        let burn_time_sec: Int?
+        let payloads: PayloadConfig?
+        
+        struct Thrust: Codable {
+            let kN: Double?
+            let lbf: Double?
+        }
+        
+        struct PayloadConfig: Codable {
+            let composite_fairing: Fairing?
+            let option_1: String?
+            
+            struct Fairing: Codable {
+                let height: Dimension?
+                let diameter: Dimension?
+                
+                struct Dimension: Codable {
+                    let meters: Double?
+                    let feet: Double?
+                }
+            }
+        }
+    }
+    
+    struct Engine: Codable {
+        let isp: ISP?
+        let thrust_sea_level: Thrust?
+        let thrust_vacuum: Thrust?
+        let number: Int?
+        let type: String?
+        let version: String?
+        let layout: String?
+        let engine_loss_max: Int?
+        let propellant_1: String?
+        let propellant_2: String?
+        let thrust_to_weight: Double?
+        
+        struct ISP: Codable {
+            let sea_level: Int?
+            let vacuum: Int?
+        }
+        
+        struct Thrust: Codable {
+            let kN: Double?
+            let lbf: Double?
+        }
+    }
+}
+
+struct Capsule: Codable, Identifiable {
+    let id: String
+    let serial: String?
+    let type: String?
+    let status: String?
+    let reuse_count: Int?
+    let water_landings: Int?
+    let land_landings: Int?
+    let last_update: String?
+    let launches: [String]?
+}
+
+struct Crew: Codable, Identifiable {
+    let id: String
+    let name: String
+    let agency: String?
+    let image: String?
+    let wikipedia: String?
+    let status: String?
+    let launches: [String]?
+}
+
+struct Payload: Codable, Identifiable {
+    let id: String
+    let name: String?
+    let type: String?
+    let mass_kg: Double?
+    let mass_lbs: Double?
+}
+
+struct Launchpad: Codable, Identifiable {
+    let id: String
+    let name: String
+    let full_name: String?
+    let locality: String?
+    let region: String?
+    let timezone: String?
+    let latitude: Double
+    let longitude: Double
+    let launch_attempts: Int?
+    let launch_successes: Int?
+    let status: String?
+    let rockets: [String]?
+    let launches: [String]?
+}
+
+struct LandingPad: Codable, Identifiable {
+    let id: String
+    let name: String
+    let full_name: String?
+    let type: String?
+    let status: String?
+    let locality: String?
+    let region: String?
+    let latitude: Double
+    let longitude: Double
+    let landing_attempts: Int?
+    let landing_successes: Int?
+    let wikipedia: String?
+    let details: String?
+    let launches: [String]?
 }
