361cddcccf91e53ff0bd3e2478bb689d85edd62c
TipsyContent <112686529+TipsyContent@users.noreply.github.com>
2025-12-03 21:49:00 +0100

    test

---
+++ Space X Project/Views/LaunchPad/LaunchpadDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchpadDetailView.swift
+//  Space X Project
+//
+//  Created by Tipsy on 03/12/2025.
+//
+
+import SwiftUI
+
+struct LaunchpadDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchpadDetailView()
+}
