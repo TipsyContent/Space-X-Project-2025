ac581e2144254040ba1280c5fa3aee22e71c2a7a
TipsyContent <112686529+TipsyContent@users.noreply.github.com>
2025-12-05 18:23:50 +0100

    sidste

--- Space X Project/Views/Auth/AuthView.swift
+++ Space X Project/Views/Auth/AuthView.swift
@@ -12,7 +12,7 @@ struct AuthView: View {
                 .bold()
                 .foregroundColor(.white)
             
-            Text("Log in or Create Accnount)
+            Text("Log in or Create Accnount")
                 .font(.caption)
                 .foregroundColor(.white.opacity(0.7))
             
